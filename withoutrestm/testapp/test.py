cd466958405cf368469c73c16ede8b66a3ede525
Anand Kumar <singhanandkr94@gmail.com>
2021-02-15 14:37:04 +0530

    handling exception from partner application

--- withoutrestm/testapp/test.py
+++ withoutrestm/testapp/test.py
@@ -6,9 +6,14 @@ ENDPOINT = "api/"
 
 def get_resource(id):
     resp = requests.get(BASE_URL+ENDPOINT+id+'/')
-    print(resp)
-    print(resp.status_code)
-    print(resp.json())
+    # if resp.status_code in range(200,300):
+    if resp.status_code == requests.codes.ok:
+        print(resp)
+        print(resp.status_code)
+        print(resp.json())
+    else:
+        print("something goes wrong")
+
 id = input("Enter id: ")
 get_resource(id)
 
