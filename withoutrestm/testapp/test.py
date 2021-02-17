cf61c8d3a9176e455839d6a7a17169d6d58c31d4
Anand Kumar <singhanandkr94@gmail.com>
2021-02-17 07:32:54 +0530

    sending data using post

--- withoutrestm/testapp/test.py
+++ withoutrestm/testapp/test.py
@@ -4,18 +4,18 @@ import requests
 BASE_URL = "http://localhost:8000/"
 ENDPOINT = "api/"
 
-def get_resource(id):
-    resp = requests.get(BASE_URL+ENDPOINT+id+'/')
-    # if resp.status_code in range(200,300):
-    # if resp.status_code == requests.codes.ok:
-    print(resp)
-    print(resp.status_code)
-    print(resp.json())
-    # else:
-    #     print("something goes wrong")
-
-id = input("Enter id: ")
-get_resource(id)
+# def get_resource(id):
+#     resp = requests.get(BASE_URL+ENDPOINT+id+'/')
+#     # if resp.status_code in range(200,300):
+#     # if resp.status_code == requests.codes.ok:
+#     print(resp)
+#     print(resp.status_code)
+#     print(resp.json())
+#     # else:
+#     #     print("something goes wrong")
+#
+# id = input("Enter id: ")
+# get_resource(id)
 
 # import requests
 #
@@ -40,3 +40,17 @@ get_resource(id)
 #     print(resp.json())
 #
 # get_all()
+import json
+BASE_URL = "http://localhost:8000/"
+ENDPOINT = "apilist/"
+def create_resource():
+    new_emp={
+        'eno':500,
+        'ename': 'shiva',
+        'esal': 5000,
+        'eaddr': 500,
+    }
+    resp=requests.post(BASE_URL+ENDPOINT, data=json.dumps(new_emp))
+    print(resp.status_code)
+    print(resp.json())
+create_resource()
