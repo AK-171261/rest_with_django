fb4cfa1a6a4cf4ec7654f8ce2e90df9a7ca277b2
Anand Kumar <singhanandkr94@gmail.com>
2021-02-22 15:05:09 +0530

    put and pathch operation

--- withoutrestm/testapp/test.py
+++ withoutrestm/testapp/test.py
@@ -1,62 +1,87 @@
-# Create your tests here.
+# # Create your tests here.
 import requests
-
 #
-BASE_URL = "http://localhost:8000/"
-ENDPOINT = "api/"
-
-# def get_resource(id):
-#     resp = requests.get(BASE_URL+ENDPOINT+id+'/')
-#     # if resp.status_code in range(200,300):
-#     # if resp.status_code == requests.codes.ok:
-#     print(resp)
-#     print(resp.status_code)
-#     print(resp.json())
-#     # else:
-#     #     print("something goes wrong")
-#
-# id = input("Enter id: ")
-# get_resource(id)
-
-# import requests
-#
-# BASE_URL = "http://localhost:8000/"
-# ENDPOINT = "apilist/"
-#
-# def get_resource():
-#     resp = requests.get(BASE_URL+ENDPOINT)
-#     print(resp.status_code)
-#     print(resp.json())
-#
-# get_resource()
-
-# import requests
-#
+# #
 # BASE_URL = "http://localhost:8000/"
 # ENDPOINT = "api/"
 #
-# def get_all():
-#     resp = requests.get(BASE_URL+ENDPOINT)
-#     print(resp.status_code)
-#     print(resp.json())
+# # def get_resource(id):
+# #     resp = requests.get(BASE_URL+ENDPOINT+id+'/')
+# #     # if resp.status_code in range(200,300):
+# #     # if resp.status_code == requests.codes.ok:
+# #     print(resp)
+# #     print(resp.status_code)
+# #     print(resp.json())
+# #     # else:
+# #     #     print("something goes wrong")
+# #
+# # id = input("Enter id: ")
+# # get_resource(id)
 #
-# get_all()
-import json
-
-BASE_URL = "http://localhost:8000/"
-ENDPOINT = "apilist/"
-
-
+# # import requests
+# #
+# # BASE_URL = "http://localhost:8000/"
+# # ENDPOINT = "apilist/"
+# #
+# # def get_resource():
+# #     resp = requests.get(BASE_URL+ENDPOINT)
+# #     print(resp.status_code)
+# #     print(resp.json())
+# #
+# # get_resource()
+#
+# # import requests
+# #
+# # BASE_URL = "http://localhost:8000/"
+# # ENDPOINT = "apilist/"
+# #
+# # def get_all():
+# #     resp = requests.get(BASE_URL+ENDPOINT)
+# #     print(resp.status_code)
+# #     print(resp.json())
+# #
+# # get_all()
+# import json
+#
+# # BASE_URL = "http://localhost:8000/"
+# # ENDPOINT = "apilist/"
+# #
+# #
 def create_resource():
     new_emp = {
-        'eno': 500,
+        'eno': 1,
         'ename': 'shiva',
-        'esal': 00,
-        'eaddr': 500,
+        'esal': 10000,
+        'eaddr': 'mizoram',
     }
     resp = requests.post(BASE_URL + ENDPOINT, data=json.dumps(new_emp))
     print(resp.status_code)
     print(resp.json())
 
 
-create_resource()
+# create_resource()
+
+BASE_URL = "http://localhost:8000/"
+ENDPOINT = "api/"
+
+
+def update_resource(id):
+    new_emp = {
+        'esal': 10000,
+        'eaddr': 'mizoram',
+    }
+    resp = requests.put(BASE_URL + ENDPOINT + str(id) + '/', data=json.dumps(new_emp))
+    print(resp.status_code)
+    print(resp.json())
+
+
+# update_resource(7)
+
+def delete_resource(id):
+    resp = requests.delete(BASE_URL + ENDPOINT + str(id) + '/')
+    print(resp.status_code)
+    print(resp.json())
+
+
+delete_resource(6)
+
--- withoutrestm/testapp/views.py
+++ withoutrestm/testapp/views.py
@@ -39,7 +39,15 @@ from .forms import EmployeeForm
 from django.core.serializers import serialize
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class EmployeeDetailCBV(HttpResponseMixin, SerializeMixin, View):
+    def get_object_by_id(self, id):
+        try:
+            emp = Employee.objects.get(id=id)
+        except Employee.DoesNotExist:
+            emp = None
+        return emp
+
     def get(self, request, id, *args, **kwargs):
         try:
             emp = Employee.objects.get(id=id)  # we got emp object from that object we will fetch desired result
@@ -47,7 +55,7 @@ class EmployeeDetailCBV(HttpResponseMixin, SerializeMixin, View):
             json_data = json.dumps({"msg": "The requested resource is not available"})
             # return HttpResponse(json_data, content_type='application/json', status=404)
             return self.render_to_http_response(json_data, status=404)
-        # emp_data = {
+        # emp_data = {@method_decorator(csrf_exempt, name="dispatch")
         #     'eno': emp.eno,
         #     'ename': emp.ename,
         #     'esal': emp.esal,
@@ -60,6 +68,49 @@ class EmployeeDetailCBV(HttpResponseMixin, SerializeMixin, View):
             # return HttpResponse(json_data, content_type='application/json', status=200)
             return self.render_to_http_response(json_data)
 
+    def put(self, request, id, *args, **kwargs):
+        # try:
+        #     emp = Employee.objects.get(id=id)
+        # except Employee.DoesNotExist:
+        #     emp = None
+        # return emp
+        emp = self.get_object_by_id(id)
+        if emp is None:
+            json_data = json.dumps({"msg": "No Matched Resource Found, Not possible to perform updation"})
+            return self.render_to_http_response(json_data, status=404)
+        data = request.body
+        valid_json = is_json(data)
+        if not valid_json:
+            json_data = json.dumps({"msg": "Please send valid json data only"})
+            return self.render_to_http_response(json_data, status=400)
+        provided_data = json.loads(data)
+        original_data = {
+            "eno": emp.eno,
+            "ename": emp.ename,
+            "esal": emp.esal,
+            "eaddr": emp.eaddr,
+        }
+        original_data.update(provided_data)
+        form = EmployeeForm(original_data, instance=emp)
+        if form.is_valid():
+            form.save(commit=True)
+            json_data = json.dumps({"msg": "Resource is updated sucessfully"})
+            return self.render_to_http_response(json_data)
+        if form.errors:
+            json_data = json.dumps(form.errors)  # form.errors is dictonary only
+            return self.render_to_http_response(json_data, status=400)
+
+    def delete(self,request,id,*args,**kwargs):
+        emp = self.get_object_by_id(id)
+        if emp is None:
+            json_data = json.dumps({"msg": "No Matched Resource Found, Not possible to perform deletion"})
+            return self.render_to_http_response(json_data, status=404)
+        status, deleted_item = emp.delete()
+        if status ==1:
+            json_data = json.dumps({"msg": "Resource deleted Sucessfully"})
+            return self.render_to_http_response(json_data)
+        json_data = json.dumps({"msg": "unable to delete"})
+        return self.render_to_http_response(json_data)
 
 @method_decorator(csrf_exempt, name="dispatch")
 class EmployeeListCBV(HttpResponseMixin, SerializeMixin, View):
